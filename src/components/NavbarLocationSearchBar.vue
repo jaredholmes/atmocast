3f63e624df6c05294ba41087e6346fa0b13be9d9
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-09-24 21:28:49 +0200

    Added component

---
+++ src/components/NavbarLocationSearchBar.vue
@@ -0,0 +1,155 @@
+<template lang="html">
+  <div class="">
+    <form @submit="preventDefaultSubmit" class="form-inline row" id="location-search-form">
+      <input v-model="searchQuery" @focus="showSearchBtn" @blur="hideSearchBtn" class="col-6 col-md-3 col-lg-9 form-control location-search-input" id="input-location-search" type="search" placeholder="Change location" aria-label="Change location">
+      <button @click="getSearchResults" class="col-2 col-lg-3 btn btn-location-search" id="btn-navbar-search" type="button" name="button">
+        <img src="icons/search.png" alt="">
+      </button>
+    </form>
+    <navbar-location-search-results
+    :searchResults="searchResults"
+    ></navbar-location-search-results>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import NavbarLocationSearchResults from './NavbarLocationSearchResults.vue';
+
+export default {
+  name: 'NavbarLocationSearchBar',
+  components: { NavbarLocationSearchResults },
+  data() {
+    return {
+      searchQuery: '',
+      searchResults: [],
+    }
+  },
+  watch: {
+    searchLocation() {
+      const searchBtn = document.getElementById('btn-navbar-search');
+      if (this.searchLocation) {
+        if (searchBtn.hasAttribute('disabled')) {
+          searchBtn.removeAttribute('disabled');
+        }
+      } else {
+        searchBtn.addAttribute('disabled');
+      }
+    }
+  },
+  methods: {
+    preventDefaultSubmit() {
+      event.preventDefault();
+      if (this.searchQuery === '') {
+        document.getElementById('input-location-search').blur();
+      } else {
+        this.getSearchResults();
+      }
+    },
+    showSearchBtn() {
+      const searchInput = document.getElementById('input-location-search');
+      const searchBtn = document.getElementById('btn-navbar-search');
+      searchBtn.style.visibility = 'visible';
+      searchBtn.style.opacity = '1';
+      searchInput.style.left = '0';
+    },
+    hideSearchBtn() {
+      this.searchResults = [];
+      if (this.searchQuery === '') {
+        const searchBtn = document.getElementById('btn-navbar-search');
+        const searchInput = document.getElementById('input-location-search');
+        searchBtn.style.opacity = '0';
+        searchBtn.style.visibility = 'hidden';
+        if (window.innerWidth >= 992) {
+          searchInput.style.left = '3em';
+        }
+      }
+    },
+    getSearchResults() {
+      axios.get('./locationSearch.json')
+          .then(response => {
+            this.searchResults = response.data;
+            console.log(this.searchResults);
+          });
+      // if (this.searchQuery) {
+      //   const requestUrl = 'https://us1.locationiq.com/v1/search.php?key='
+      //   + '834b5e16cebecd&q='
+      //   + this.searchQuery
+      //   + '&format=json'
+      //   axios.get(requestUrl)
+      //     .then(response => {
+      //       this.searchResults = response.data;
+      //     })
+      // }
+    }
+  },
+  beforeMount() {
+    if (this.searchLocation) {
+      if (searchBtn.hasAttribute('disabled')) {
+        searchBtn.removeAttribute('disabled');
+      }
+    }
+  },
+}
+</script>
+
+<style scoped lang="sass">
+  @import "../stylesheets/styles"
+
+  .location-search-input
+    background-color: rgba(0, 0, 0, 0)
+    transition: background-color 300ms linear, left 250ms
+    -webkit-transition: background-color 300ms linear, left 250ms
+    -ms-transition: background-color 300ms linear, left 250ms
+    border: none
+    padding: $s-s-6
+    position: relative
+
+    @include media-large
+      left: 3em
+      max-width: 75%
+
+  .location-search-input:hover
+    cursor: pointer
+
+  .location-search-input::placeholder
+    color: $text-primary
+    font-weight: 400
+    text-align: left
+    transition: left 0.25s
+    -webkit-transition: left 0.25s
+    -ms-transition: left 0.25s
+
+    @include media-large
+      text-align: left
+
+  .location-search-input:focus
+    background-color: $light
+    box-shadow: none
+    left: 0
+
+  .location-search-input:focus:hover
+    cursor: text
+
+  .location-search-input:focus::placeholder
+    color: $text-secondary
+    font-weight: 300
+
+  .btn-location-search
+    max-width: 3em
+    visibility: hidden
+    opacity: 0
+    transition: opacity 800ms
+    -webkit-transition: opacity 800ms
+    -ms-transition: opacity 800ms
+    padding: 0
+    border: none
+    background-color: rgba(0, 0, 0, 0)
+
+    @include media-large
+    img
+      max-width: 1.5em
+
+    img:hover
+      opacity: 0.6
+</style>
---
+++ src/components/NavbarLocationSearchResults.vue
@@ -0,0 +1,57 @@
+<template lang="html">
+  <div v-if="searchResults.length > 0" class="row">
+    <ul class="list-group">
+      <button @click="searchResults = []" class="btn close-search-results fw-semi"type="button" name="button">X</button>
+      <li v-for="result in searchResults" @click="getD(result)" class="list-group-item">
+        {{ result.display_name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarLocationSearchResults',
+  props: ['searchResults'],
+  methods: {
+    getD(result) {
+      console.log(result);
+    }
+  },
+}
+</script>
+
+<style scoped lang="sass">
+  @import "../stylesheets/styles.sass"
+
+  .row
+    height: $s-l-6
+    position: absolute
+
+  .row *
+    z-index: 1
+
+  .list-group
+    max-width: 100%
+    height: auto
+    overflow-y: scroll
+    position: relative
+
+    @include media-large
+      max-width: $s-l-6
+
+  .list-group-item
+    overflow: visible
+
+  .list-group-item:hover
+    cursor: pointer
+
+  .close-search-results
+    border: none
+    background-color: rgba(0, 0, 0, 0)
+    position: sticky
+    top: 1em
+    text-align: right
+    z-index: 2
+
+</style>

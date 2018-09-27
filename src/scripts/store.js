373c1491a6997e6e8958c7f4288786694d96d6d3
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-09-27 10:54:17 +0200

    Added Vuex store

---
+++ src/scripts/store.js
@@ -0,0 +1,55 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+// Vuex config
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+  state: {
+    metric: true,
+    weather: {},
+    coords: {
+      lat: '',
+      lon: '',
+    },
+    navbarSearchResults: [],
+  },
+  mutations: {
+    toggleMetric(state) {
+      if (state.metric) {
+        state.metric = false;
+      } else {
+        state.metric = true;
+      }
+    },
+    setWeather(state, weatherData) {
+      state.weather = weatherData.weatherData;
+    },
+    setCoords(state, coords) {
+      state.coords = coords.coords;
+    },
+    setNavbarSearchResults(state, results) {
+      state.navbarSearchResults = results.results;
+    },
+  },
+  getters: {
+    currentWeather: state => {
+      return state.weather.currently;
+    },
+    currentIcon: state => {
+      return state.weather.currently.icon;
+    },
+    hourlyWeather: state => {
+      return state.weather.hourly.data;
+    },
+    dailyWeather: state => {
+      return state.weather.daily.data;
+    },
+    latitude: state => {
+      return state.coords.lat;
+    },
+    longitude: state => {
+      return state.coords.lon;
+    },
+  },
+});

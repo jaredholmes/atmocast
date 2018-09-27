b3601017dc0adee462d71a055be84ea5623ea0ab
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-09-27 10:54:00 +0200

    Added Vuex

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,8 +4,12 @@ import moment from 'moment';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
+import { store } from './store';
 import '../stylesheets/styles.sass';
 import IndexPage from '../components/IndexPage.vue';
+
+// Router config
+Vue.use(VueRouter);
 
 const routes = [
   { path: '', component: IndexPage },
@@ -16,15 +20,17 @@ const router = new VueRouter({
   routes,
 })
 
-Vue.use(VueRouter);
-
 Vue.mixin ({
-  data() {
-    return {
-      $metricUnits: true,
-    }
-  },
   methods: {
+    $hideNavbarSearchButton() {
+      const searchBtn = document.getElementById('btn-navbar-search');
+      const searchInput = document.getElementById('input-location-search');
+      searchBtn.style.opacity = '0';
+      searchBtn.style.visibility = 'hidden';
+      if (window.innerWidth >= 992) {
+        searchInput.style.left = '3em';
+      }
+    },
     $momentUnixHour(unix, index) {
       if (index === 0) {
         return 'Now';
@@ -41,19 +47,25 @@ Vue.mixin ({
         return moment().add(days, 'days').format('dddd');
       }
     },
+    // Imperial to metric conversions
     $fToC(f) {
       return (f - 32) * 5/9;
     },
     $mToKm(m) {
       return m * 1.609344;
     },
+    // Metric to imperial conversions
+    $cToF(c) {
+      return c * 9/5 + 32;
+    },
+    $kmToM(km) {
+      return km / 1.609344;
+    }
   }
 })
 
 new Vue({
   el: '#app',
-  data: {
-    metric: true
-  },
   router,
+  store,
 }).$mount('#app');

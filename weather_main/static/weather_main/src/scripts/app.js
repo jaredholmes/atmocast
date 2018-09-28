49d219c1239342e74de8cee688265300e049d52d
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-09-28 11:13:38 +0200

    Changed entire Bootstrap import to select Bootstrap files only

--- weather_main/static/weather_main/src/scripts/app.js
+++ weather_main/static/weather_main/src/scripts/app.js
@@ -2,7 +2,11 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import moment from 'moment';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
+// import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/js/dist/button';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/util';
 
 import { store } from './store';
 import '../stylesheets/styles.sass';

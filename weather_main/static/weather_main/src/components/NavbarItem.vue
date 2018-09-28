d54c1ed2544892777202f0af59a87297bc9e4971
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-09-28 11:12:55 +0200

    Added logo

--- weather_main/static/weather_main/src/components/NavbarItem.vue
+++ weather_main/static/weather_main/src/components/NavbarItem.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <nav class="navbar navbar-expand-lg" id="navbar-main">
     <div class="nav-wrapper row">
-      <a href="/" class="navbar-brand col-4 fw-semi fs-large">Atmocast</a>
+      <a href="/" class="navbar-brand col-4 fw-semi fs-large">Atmocast <img class="nav-logo" :src="$store.state.iconLocationPrefix + 'logo.png'" alt=""> </a>
       <button class="ml-auto navbar-toggler col-1 offset-5" data-toggle="collapse" data-target="#nav-collapse" aria-controls="nav-collapse" aria-expanded="false" aria-label="Toggle navigation">
         <img class="icon icon-menu" :src="$store.state.iconLocationPrefix + 'menu.png'" alt="">
       </button>
@@ -127,6 +127,10 @@ export default {
 
     @include media-large
       min-width: 0
+
+    .nav-logo
+      max-width: $s-s-1
+      padding-bottom: 3px
 
   .navbar-collapse
     float: right

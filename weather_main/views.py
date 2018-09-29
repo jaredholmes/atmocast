08c625d48cc02e95f2425ec829f4ce653832e859
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-09-29 12:11:19 +0200

    Changed 404 page to redirect to index

--- weather_main/views.py
+++ weather_main/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponseRedirect, reverse
 
 # Create your views here.
 def index(request):
     return render(request, 'weather_main/index.html')
 
 def not_found(request):
-    return HttpResponse('Not found')
+    return HttpResponseRedirect(reverse('home'))

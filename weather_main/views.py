1d5db3fd4025ac85981cf49f14df661658fbedd5
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-10-02 13:43:05 +0200

    Added HTTP to HTTPS redirect

--- weather_main/views.py
+++ weather_main/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.shortcuts import render, HttpResponseRedirect, reverse, HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'weather_main/index.html')
+    # return HttpResponse(request.build_absolute_uri());
+    if request.is_secure() or 'http://localhost' in request.build_absolute_uri():
+        return render(request, 'weather_main/index.html')
+    else:
+        return HttpResponseRedirect('https://atmocast.com')
 
 def not_found(request):
     return HttpResponseRedirect(reverse('home'))

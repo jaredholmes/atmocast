62728c9ac087255b0bf0ba498040672cab03a57c
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-10-05 17:05:45 +0200

    Added proxy view and URL for DarkSky API

--- weather_main/urls.py
+++ weather_main/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
+    re_path(r'^weather_data/(?P<lat>[-]?[0-9]{1,2}\.{1}[0-9]{1,6})/(?P<lon>[-]?[0-9]{1,3}\.{1}[0-9]{1,6})/$', views.weather_data),
     path('sw.js', (TemplateView.as_view(template_name="sw.js", content_type="application/javascript")), name="service_worker"),
     re_path(r'^.*$', views.not_found, name="not_found"),
 ]
--- weather_main/views.py
+++ weather_main/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse, HttpResponse
-# from django.views.decorators.gzip import gzip_page
+import requests
 
 # Create your views here.
-# @gzip_page
 def index(request):
-    # return HttpResponse(request.build_absolute_uri());
     if request.is_secure() or 'http://localhost' in request.build_absolute_uri():
         return render(request, 'weather_main/index.html')
     else:
         return HttpResponseRedirect('https://atmocast.com')
+
+def weather_data(request, lat, lon):
+    if lat and lon:
+        darkSkyURL = 'https://api.darksky.net/forecast/c11e01133c18e01ad3d20c3aeb1a9218/' + lat + ',' + lon
+        darkSkyParams = { 'exclude': 'currently,minutely,alerts,flags' }
+        response = requests.get(darkSkyURL, params=darkSkyParams)
+    else:
+        response = 'Invalid co-ordinates given in request.'
+
+    return HttpResponse(response)
 
 def not_found(request):
     return HttpResponseRedirect(reverse('home'))

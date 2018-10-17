983b73d86f89bb10028eab203f724cc4c4a8d43d
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-10-17 11:04:07 +0200

    Added all icons to cache

--- weather_main/templates/sw.js
+++ weather_main/templates/sw.js
@@ -7,7 +7,7 @@ self.addEventListener('install', function(event) {
   event.waitUntil(
     fetch(offlinePage).then(function(response) {
       return caches.open('pwabuilder-offline').then(function(cache) {
-        console.log('[PWA Builder] Cached offline page during Install'+ response.url);
+        // console.log('[PWA Builder] Cached offline page during Install'+ response.url);
         return cache.put(offlinePage, response);
       });
   }));
@@ -16,9 +16,39 @@ self.addEventListener('install', function(event) {
     caches.open('atmocache')
       .then(cache => {
         return cache.addAll([
+          // Bundles
           'https://atmocast.com/static/weather_main/www/static/weather_main/dist/bundles/app.bundle.js',
           'https://atmocast.com/static/weather_main/www/static/weather_main/dist/bundles/spinnerStyles.bundle.js',
+          // Images
           'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons/logo-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons/close-dark.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons/close-light.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons/delete.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons/heart-filled.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons/heart-outline.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons/location.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons/menu.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons/search.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/clear-day-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/clear-day-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/clear-night-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/clear-night-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/cloudy-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/cloudy-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/fog-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/fog-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/partly-cloudy-day-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/partly-cloudy-day-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/partly-cloudy-night-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/partly-cloudy-night-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/rain-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/rain-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/sleet-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/sleet-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/snow-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/snow-small.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/wind-large.png',
+          'https://atmocast.com/static/weather_main/www/static/weather_main/dist/icons//custom/wind-small.png',
         ])
       })
   );
@@ -29,7 +59,7 @@ self.addEventListener('install', function(event) {
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     fetch(event.request).catch(function(error) {
-      console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
+      // console.error( '[PWA Builder] Network request Failed. Serving offline page ' + error );
       return caches.open('pwabuilder-offline').then(function(cache) {
         return cache.match('offline.html');
       });
@@ -40,7 +70,7 @@ self.addEventListener('fetch', function(event) {
 //This is a event that can be fired from your page to tell the SW to update the offline page
 self.addEventListener('refreshOffline', function(response) {
   return caches.open('pwabuilder-offline').then(function(cache) {
-    console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
+    // console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
     return cache.put(offlinePage, response);
   });
 });

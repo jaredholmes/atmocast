518af4890800cd71ff756dd70a758dce4d46d788
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-11-11 11:37:06 +0200

    New Android build

--- weather_main/static/weather_main/platforms/android/app/build/generated/source/buildConfig/release/com/atmocast/app/BuildConfig.java
+++ weather_main/static/weather_main/platforms/android/app/build/generated/source/buildConfig/release/com/atmocast/app/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "com.atmocast.app";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 11000;
-  public static final String VERSION_NAME = "1.1.0";
+  public static final int VERSION_CODE = 11100;
+  public static final String VERSION_NAME = "1.1.1";
 }

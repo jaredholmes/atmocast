75fda5e0ec20a0914ecb8db7fe89377f542b5516
Jared Holmes <jar3dh0lm3s@gmail.com>
2018-11-12 10:21:45 +0200

    Added cordova-plugin-inappbrowser

--- weather_main/static/weather_main/platforms/android/platform_www/cordova_plugins.js
+++ weather_main/static/weather_main/platforms/android/platform_www/cordova_plugins.js
@@ -23,11 +23,12 @@ module.exports = [
     ]
   },
   {
-    "id": "cordova-plugin-device.device",
-    "file": "plugins/cordova-plugin-device/www/device.js",
-    "pluginId": "cordova-plugin-device",
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
     "clobbers": [
-      "device"
+      "cordova.InAppBrowser.open",
+      "window.open"
     ]
   }
 ];
@@ -37,7 +38,7 @@ module.exports.metadata =
   "cordova-plugin-geolocation": "4.0.1",
   "cordova-plugin-whitelist": "1.3.3",
   "cordova-plugin-statusbar": "2.4.2",
-  "cordova-plugin-device": "2.0.2"
+  "cordova-plugin-inappbrowser": "3.0.0"
 };
 // BOTTOM OF METADATA
 });
